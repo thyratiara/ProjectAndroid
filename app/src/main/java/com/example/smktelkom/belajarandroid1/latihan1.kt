078e7f53e9a2f70a2f085e55bd337be372d4c95f
SMK TELKOM <tiara_dewi_25rpl@student.smktelkom-mlg.sch.id>
2018-01-08 22:11:30 +0700

    Project Android

---
+++ app/src/main/java/com/example/smktelkom/belajarandroid1/latihan1.kt
@@ -0,0 +1,12 @@
+package com.example.smktelkom.belajarandroid1
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class latihan1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_latihan1)
+    }
+}
